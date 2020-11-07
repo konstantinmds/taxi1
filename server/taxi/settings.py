41e44a3d84caf5c45ccf45415e43b2bc23a55769
kosta <konstantin@bitracks.com>
2020-11-07 01:47:32 +0100

    tests

--- server/taxi/settings.py
+++ server/taxi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import datetime
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,7 +77,6 @@ WSGI_APPLICATION = 'taxi.wsgi.application'
 
 ASGI_APPLICATION = 'taxi.routing.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -141,4 +141,17 @@ CHANNEL_LAYERS = {
             'hosts': [REDIS_URL]
         }
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'USER_ID_CLAIM': 'id',
 }
