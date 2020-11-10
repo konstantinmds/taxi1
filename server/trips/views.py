1eaea6c335cfd4be9f1bb9a93e102527d84f6fdc
kosta <konstantin@bitracks.com>
2020-11-10 13:41:38 +0100

    serializers trip tests

--- server/trips/views.py
+++ server/trips/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics, permissions, viewsets
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .serializers import UserSerializer, LogInSerializer, TripSerializer
+from .models import Trip
+
+class SignUpView(generics.CreateAPIView):
+  queryset = get_user_model().objects.all()
+  serializer_class = UserSerializer
+
+class LogInView(TokenObtainPairView):
+  serializer_class = LogInSerializer
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+  permission_classes = (permissions.IsAuthenticated,)
+  queryset = Trip.objects.all()
+  serializer_class = TripSerializer
